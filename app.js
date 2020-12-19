19ef5f855e6eb87febbb03d5fcae85cb4c0430bc
Nahid Hasan <rajunahidhasan0@gmail.com>
2020-12-20 02:24:04 +0600

    cdoe for create a table on database

--- app.js
+++ app.js
@@ -28,11 +28,11 @@ app.get('/createdb' ,(req,res)=>{
         res.send('Database created...');
     });
 });
-
+ 
 //create table
 app.get('/createpoststable', (req, res) =>{
     let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))';
-    db.query(sql,(err, result)=>{
+    db.query(sql,(err, result) => {
         if(err)throw err;
         console.log(result);
         res.send('Posts table created...');
