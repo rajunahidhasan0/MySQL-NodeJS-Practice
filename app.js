d7523fac0be91f0f83b9b6b780a24690ca96fc87
Nahid Hasan <rajunahidhasan0@gmail.com>
2020-12-20 02:22:47 +0600

    create table "posts"

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const mysql = require('mysql');
 const db = mysql.createConnection({
    host     : 'localhost',
    user     : 'root',
-   password : '' 
+   password : '',
+   database: 'nodemysql'
 });
 
 //connect
@@ -25,6 +26,16 @@ app.get('/createdb' ,(req,res)=>{
         if(err)throw err;
         console.log(result);
         res.send('Database created...');
+    });
+});
+
+//create table
+app.get('/createpoststable', (req, res) =>{
+    let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))';
+    db.query(sql,(err, result)=>{
+        if(err)throw err;
+        console.log(result);
+        res.send('Posts table created...');
     });
 });
 
