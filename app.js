22b9018f4946afd91135fc6d80b40cfc210caced
Nahid Hasan <rajunahidhasan0@gmail.com>
2020-12-20 02:38:04 +0600

    code for Insert data into the table

--- app.js
+++ app.js
@@ -39,6 +39,17 @@ app.get('/createpoststable', (req, res) =>{
     });
 });
 
+// Insert post 1
+app.get('/addpost1',(req, res)=>{
+    let post={title:'Post One', body:'This is post number 1'};
+    let sql='INSERT INTO posts SET ?';
+    let query=db.query(sql,post,(err, result) => {
+        if(err)throw err;
+        console.log(result);
+        res.send('Post 1 added...');
+    });
+});
+
 app.listen('3000', () => {
     console.log('Server started on port 3000');
 });
