330861e27fd91c2ed06a3bcdce4b1efbeeba0b37
Nahid Hasan <rajunahidhasan0@gmail.com>
2020-12-20 01:54:09 +0600

    create database "mysql2" from app.js

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const mysql = require('mysql');
 
+//Create connection
+const db = mysql.createConnection({
+   host     : 'localhost',
+   user     : 'root',
+   password : '' 
+});
+
+//connect
+db.connect((err)=>{
+    if(err){
+        throw err;
+    }
+    console.log('Mysql Connected...')
+});
+
 const app = express();
 
-app.listen('3306', () => {
-    console.log('Server started on port 3306');
+//create DB
+app.get('/createdb' ,(req,res)=>{
+    let sql = 'CREATE DATABASE nodemysql2';
+    db.query(sql, (err, result)=>{
+        if(err)throw err;
+        console.log(result);
+        res.send('Database created...');
+    });
+});
+
+app.listen('3000', () => {
+    console.log('Server started on port 3000');
 });
